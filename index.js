1f8b34917d6f08bcf76e6788eecce051e5f41431
samalcott <94107393+samalcott@users.noreply.github.com>
2021-11-11 09:18:16 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hello world ');
+
+
+
+function sayhello(name){
+    return "hello", name
+}
+
+console.log(sayhello('sam'))
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+console.log(add(5,6))
